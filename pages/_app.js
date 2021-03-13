75bfd30ea783adf741bc9fd3da49af01726686f0
edutanaka <edutanakabr@gmail.com>
2021-03-13 14:42:39 -0300

    projeto inicial

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,22 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import GlobalStyle from '../src/theme/GlobalStyle';
+import theme from '../src/theme';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@100&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
---
+++ src/theme/index.js
@@ -0,0 +1,20 @@
+
+export const colors = {
+  
+};
+
+export const breakpoints = {
+  xs: 0,
+  sm: 480,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+};
+
+export default {
+  colors,
+  breakpoints,
+  borderRadius: '12px',
+  transition: '200ms ease-in-out',
+  fontFamily: '\'Fira Sans Condensed\', sans-serif',
+};
